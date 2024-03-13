86df82019b8a9b3f9eac2713462a59866c4ece5d
chaujulien <chaujulien01@gmail.com>
2024-03-13 12:09:20 -0400

    Complete budget app version

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,16 +2,40 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { dispatch, budget, expenses, currency } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total += item.cost);
+        }, 0);
+
+        const inputBudget = event.target.value;
+
+        if (inputBudget > 20000) {
+            alert("The budget cannot exceed £20.000 !");
+            return;
+        }
+
+        if (inputBudget < totalExpenses) {
+            alert("The budget cannot be lower thant the spending !");
+            return;
+        }
+
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: event.target.value,
+        });
     }
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span>Budget: {currency}
+                <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+                </span>
+        </div>
     );
 };
+
 export default Budget;
